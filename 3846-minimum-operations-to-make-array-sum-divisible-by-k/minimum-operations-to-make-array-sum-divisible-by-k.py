22186710f51ebe50a0138a3daa59b59aa7e7f0b4
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-29 06:44:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (72.61%) - LeetSync

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.py
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.py
@@ -1,28 +1,3 @@
 class Solution(object):
     def minOperations(self, nums, k):
-        diff=0
-        count=0
-        new_diff=None
-        max_num=sum(nums)
-        if sum(nums) == k:
-            return 0 
-        if sum(nums) < k:
-            return sum(nums)
-        if sum(nums) > k:
-            for i in range(len(nums)):
-                if len(nums)>1:
-                    diff=sum(nums)-nums[i]
-                    if new_diff is None or diff > new_diff:
-                        new_diff=diff
-                else:
-                    new_diff=sum(nums)
-        
-        for j in range(1,new_diff):
-            
-            if max_num % k == 0:
-                return count
-            else:
-                max_num -=1
-                count +=1
-        
-        return count
+        return (sum(nums)%k)

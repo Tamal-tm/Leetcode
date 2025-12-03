65b660f413240ac30ff664ed2899c4cac5264053
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-03 06:12:03 +0530

    Time: 14 ms (91.11%) | Memory: 12.3 MB (100.00%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -1,28 +1,10 @@
 class Solution(object):
     def totalNumbers(self, digits):
-        res = []
-        count=0
-        used = [0]*10
-        for d in digits:
-            used[d] += 1   # frequency table
+        num=set()
 
-        # Hundreds digit: 1–9 (cannot be 0)
-        for a in range(1, 10):
-            if used[a] == 0:
-                continue
-            used[a] -= 1
-
-            # Tens digit: 0–9
-            for b in range(10):
-                if used[b] == 0:
-                    continue
-                used[b] -= 1
-
-                # Units digit: must be even
-                for c in (0,2,4,6,8):
-                    if used[c] > 0:
-                        res.append(a*100 + b*10 + c)
-                        count +=1
-                used[b] += 1
-            used[a] += 1
-        return count
+        for a,b,c in permutations(digits,3):
+            if a!=0 and c%2==0:
+                num.add((a,b,c))
+        
+        return len(num)
+        

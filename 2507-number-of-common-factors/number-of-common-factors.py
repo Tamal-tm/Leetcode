5a4317701e535e9b956192a481ea2d983fd3a017
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-04 10:06:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (14.33%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.py
+++ 2507-number-of-common-factors/number-of-common-factors.py
@@ -1,15 +1,19 @@
-
 class Solution(object):
     def commonFactors(self, a, b):
-        def gcd(a,b):
-            count=0
-            if a>b:
-                return gcd(b,a)
-            else:
-                for i in range(1,a+1):
-                    if b % i == 0 and a % i ==0:
-                        count +=1
-                return count
-            
-        res=gcd(a,b)
-        return (res)
+        def gcd(a, b):
+            if a > b:
+                return gcd(b, a)
+
+            count = 0
+            i = 1
+            while i * i <= a:        # loop only till sqrt(a)
+                if a % i == 0:
+                    if b % i == 0:
+                        count += 1
+                    j = a // i       # paired factor
+                    if j != i and b % j == 0:
+                        count += 1
+                i += 1
+            return count
+
+        return gcd(a, b)

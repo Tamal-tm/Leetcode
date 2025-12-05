50066c6dae1b1ee7a79b7507ecc6a38c0df9b065
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-05 06:22:54 +0530

    Time: 206 ms (64.54%) | Memory: 12.3 MB (87.62%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.py
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.py
@@ -1,20 +1,15 @@
 class Solution(object):
     def hasSameDigits(self, s):
 
-        def repeat(num):
-            new_num = str(num)
-            result = ""
-            count = 0
+        def reduce_num(x):
+            x = str(x)
+            while len(x) > 2:          # keep reducing until only 2 digits left
+                nxt = []
+                for i in range(len(x) - 1):
+                    val = (int(x[i]) + int(x[i+1])) % 10
+                    nxt.append(str(val))
+                x = "".join(nxt)
+            return x
 
-            for i in range(len(new_num) - 1):
-                value = (int(new_num[i]) + int(new_num[i+1])) % 10
-                result += str(value)
-                count += 1
-
-            if count > 2:
-                return repeat(result)
-            else:
-                return result
-
-        res = repeat(s)
-        return res[0] == res[1]
+        final = reduce_num(s)
+        return final[0] == final[1]

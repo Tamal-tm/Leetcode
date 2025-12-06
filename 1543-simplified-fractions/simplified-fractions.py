50619da40ec2b708c69e73de476c315674f836a5
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-06 06:49:49 +0530

    Time: 58 ms (90.91%) | Memory: 12.8 MB (68.18%) - LeetSync

--- 1543-simplified-fractions/simplified-fractions.py
+++ 1543-simplified-fractions/simplified-fractions.py
@@ -1,27 +1,20 @@
 class Solution(object):
     def simplifiedFractions(self, n):
         mylist = []
-        a = 0   # you used it for shifting start of i
-        c = 0   # flag to mark "has common divisor"
+        a = 0
+
+        # Proper gcd function (still similar structure)
+        def gcd(x, y):
+            while y:
+                x, y = y, x % y
+            return x
 
         for j in range(1, n):
-            
             for i in range(2 + a, n + 1):
-
-                # check gcd(j, i)
-                def gcd(x, y):
-                    bx = min(x, y)
-                    for b in range(2, bx + 1):
-                        if x % b == 0 and y % b == 0:
-                            return True   # common divisor found
-                    return False
-
-                if gcd(j, i):   # if not simplified
-                    c = 0       # reset flag, skip
-                else:
+                # Only add if fraction is simplified
+                if gcd(j, i) == 1:
                     new_item = str(j) + "/" + str(i)
                     mylist.append(new_item)
-
-            a += 1   # keep your structure
+            a += 1
 
         return mylist

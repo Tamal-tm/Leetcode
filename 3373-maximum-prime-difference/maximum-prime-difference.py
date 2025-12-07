96207cc32080f7c541e6d41165ebe37a4a522aad
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-07 07:00:53 +0530

    Time: 1143 ms (37.50%) | Memory: 28.7 MB (40.63%) - LeetSync

--- 3373-maximum-prime-difference/maximum-prime-difference.py
+++ 3373-maximum-prime-difference/maximum-prime-difference.py
@@ -1,26 +1,26 @@
 class Solution(object):
     def maximumPrimeDifference(self, nums):
+        checkPrime = True
         mylist = []
 
         for i in range(len(nums)):
             val = nums[i]
 
-            # Skip 1 immediately
             if val <= 1:
-                continue
+                checkPrime = False
+            else:
+                # optimised prime check (sqrt)
+                for j in range(2, int(val**0.5) + 1):
+                    if val % j == 0:
+                        checkPrime = False
+                        break
 
-            # Prime check (optimized)
-            isPrime = True
-            for j in range(2, int(val**0.5) + 1):   # check only till sqrt(val)
-                if val % j == 0:
-                    isPrime = False
-                    break
-
-            # If prime → record index
-            if isPrime:
+            if checkPrime == True:
                 mylist.append(i)
 
-        # Return difference
+            checkPrime = True   # reset
+
         if len(mylist) <= 1:
             return 0
-        return mylist[-1] - mylist[0]
+        else:
+            return mylist[-1] - mylist[0]

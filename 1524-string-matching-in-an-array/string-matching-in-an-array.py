a40c8419fb13edc7d1e051126c282ee2449ad9c9
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-09 07:16:41 +0530

    Time: 3 ms (79.30%) | Memory: 12.5 MB (8.20%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,18 +1,11 @@
 class Solution(object):
     def stringMatching(self, words):
-        mylist = []
-        length = len(words)
-
-        # Sort once → biggest words at the end
-        words.sort(key=len)
-
-        # Same structure: use nested loops, use big_word each round
-        for i in range(length):
-            big_word = words[-1]          # largest remaining
-            for word in words[:-1]:       # check only smaller words
-                if word in big_word and word not in mylist:
-                    mylist.append(word)
-            
-            words.pop()                   # remove big_word (same structure)
+        res=[]
+        n=len(words)
+        for i in range(n):
+            for j in range(n):
+                if i!=j and words[i] in words[j]:
+                    res.append(words[i])
+                    break
+        return res
         
-        return mylist

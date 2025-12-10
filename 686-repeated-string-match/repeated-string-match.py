e6d0a4ce61e0667f7f7fb065ecb21b02cb2ab69d
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-10 06:54:35 +0530

    Time: 71 ms (84.62%) | Memory: 12.6 MB (71.95%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.py
+++ 686-repeated-string-match/repeated-string-match.py
@@ -1,16 +1,8 @@
 class Solution(object):
     def repeatedStringMatch(self, a, b):
-        value_1 = len(b)
-        value_2 = len(a)
+        base = len(b) // len(a)
 
-        short = value_1 // value_2
-
-        
-        if b in (a * short):
-            return short
-        if b in (a * (short + 1)):
-            return short + 1
-        if b in (a * (short + 2)):
-            return short + 2
-
+        for x in (base, base+1, base+2):
+            if b in a * x:
+                return x
         return -1

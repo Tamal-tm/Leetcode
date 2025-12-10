a886f894215a9453d11e362f0f38159a8a4ff6bd
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-10 06:34:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (50.45%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -1,18 +1,5 @@
 class Solution(object):
     def repeatedSubstringPattern(self, s):
-        n = len(s)
-        
-        # Try substring lengths from 1 to n//2
-        for value in range(1, n//2 + 1):
-
-            # Only check if value divides n
-            if n % value == 0:
-
-                # Take substring
-                substring = s[:value]
-
-                # Repeat it n/value times
-                if substring * (n // value) == s:
-                    return True
-
-        return False
+        # Key trick: if s is made of repeats,
+        # it will appear inside (s+s)[1:-1]
+        return s in (s + s)[1:-1]

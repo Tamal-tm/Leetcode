ed28adaf2219989511e663995d3315efeac7964f
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-12 06:36:44 +0530

    Time: 12 ms (47.34%) | Memory: 16.1 MB (28.56%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,22 +1,20 @@
 class Solution(object):
     def majorityElement(self, nums):
-        seen={}
-        mylist=[]
-        if len(nums) == 2 and nums[0]!=nums[1]:
-            return nums
-        elif len(nums)<3:
-            mylist.append(nums[0])
-            return mylist
-        else:
-            val=(len(nums)/3)
-            for i in range(len(nums)):
-                if nums[i] in seen:
-                    seen[nums[i]] +=1
-                else:
-                    seen[nums[i]] = 1
+        seen = {}
+        mylist = []
 
-            for ch in seen:
-                if seen[ch]>val:
-                    mylist.append(ch)
-                
+        # frequency count
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                seen[nums[i]] += 1
+            else:
+                seen[nums[i]] = 1
+
+        limit = len(nums) // 3   # threshold
+
+        # collect elements appearing more than n/3
+        for key in seen:
+            if seen[key] > limit:
+                mylist.append(key)
+
         return mylist

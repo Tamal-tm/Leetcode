15b381803b632be7672054bdba6866f8658a44f6
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-16 05:37:42 +0530

    Time: 4 ms (66.22%) | Memory: 12.5 MB (46.09%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,23 +1,16 @@
 class Solution(object):
     def maxNumberOfBalloons(self, text):
-        letters = "balloon"
         seen = {}
-        count = 0
-        real_count = 0
 
-        # count letters
-        for i in range(len(text)):
-            if text[i] in letters:
-                if text[i] in seen:
-                    seen[text[i]] += 1
-                else:
-                    seen[text[i]] = 1
+        # Count characters
+        for ch in text:
+            seen[ch] = seen.get(ch, 0) + 1
 
-        # keep forming "balloon"
-        while True:
-            for ch in letters:
-                if ch not in seen or seen[ch] == 0:
-                    return real_count
-                seen[ch] -= 1
-
-            real_count += 1
+        # Calculate how many "balloon"s we can form
+        return min(
+            seen.get('b', 0),
+            seen.get('a', 0),
+            seen.get('l', 0) // 2,
+            seen.get('o', 0) // 2,
+            seen.get('n', 0)
+        )

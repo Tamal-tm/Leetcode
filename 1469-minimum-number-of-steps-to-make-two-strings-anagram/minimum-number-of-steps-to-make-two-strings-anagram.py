702cdebd8eef9af1e9a295d6b1833535df70af57
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-17 06:43:14 +0530

    Time: 194 ms (53.93%) | Memory: 14.7 MB (5.99%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,11 +1,20 @@
 class Solution(object):
     def minSteps(self, s, t):
-        count=0
-        for i in range(len(t)):
-            if t[i] in s:
-                index_pos=s.index(t[i])
-                s = s[:index_pos] + s[index_pos+1:]
+        count = 0
+        seen = {}
+
+        # build frequency map for s
+        for ch in s:
+            if ch in seen:
+                seen[ch] += 1
             else:
-                count +=1
-        return (count)
-        
+                seen[ch] = 1
+
+        # process t
+        for i in range(len(t)):
+            if t[i] in seen and seen[t[i]] > 0:
+                seen[t[i]] -= 1
+            else:
+                count += 1
+
+        return count

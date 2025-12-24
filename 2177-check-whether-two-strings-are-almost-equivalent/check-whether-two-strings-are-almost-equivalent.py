fb7a128d606db83b10d0946eb55619f01cfe94d0
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-24 06:55:57 +0530

    Time: 3 ms (52.86%) | Memory: 12.6 MB (10.00%) - LeetSync

--- 2177-check-whether-two-strings-are-almost-equivalent/check-whether-two-strings-are-almost-equivalent.py
+++ 2177-check-whether-two-strings-are-almost-equivalent/check-whether-two-strings-are-almost-equivalent.py
@@ -1,17 +1,26 @@
 class Solution(object):
     def checkAlmostEquivalent(self, word1, word2):
-        freq1 = {}
-        freq2 = {}
+        seen_1 = {}
+        seen_2 = {}
 
-        for ch in word1:
-            freq1[ch] = freq1.get(ch, 0) + 1
-
-        for ch in word2:
-            freq2[ch] = freq2.get(ch, 0) + 1
-
-        for i in range(26):
-            c = chr(ord('a') + i)
-            if abs(freq1.get(c, 0) - freq2.get(c, 0)) > 3:
+        for i in range(len(word1)):
+            if word1[i] in seen_1:
+                seen_1[word1[i]] += 1
+            else:
+                seen_1[word1[i]] = 1
+        
+        for j in range(len(word2)):
+            if word2[j] in seen_2:
+                seen_2[word2[j]] += 1
+            else:
+                seen_2[word2[j]] = 1
+                
+        for key in seen_1:
+            if abs(seen_1.get(key, 0) - seen_2.get(key, 0)) > 3:
                 return False
-
+        
+        for key in seen_2:
+            if abs(seen_2.get(key, 0) - seen_1.get(key, 0)) > 3:
+                return False
+        
         return True

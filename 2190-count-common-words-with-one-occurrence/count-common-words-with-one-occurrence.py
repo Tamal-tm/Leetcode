6001f1e202aa1298294df81ec11793762d79b870
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-25 06:31:51 +0530

    Time: 7 ms (60.83%) | Memory: 12.8 MB (51.67%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.py
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.py
@@ -1,23 +1,17 @@
 class Solution(object):
     def countWords(self, words1, words2):
-        count = 0
         seen1 = {}
         seen2 = {}
+        count = 0
 
-        for word in words1:
-            if word in seen1:
-                seen1[word] += 1
-            else:
-                seen1[word] = 1
+        for w in words1:
+            seen1[w] = seen1.get(w, 0) + 1
 
-        for word in words2:
-            if word in seen2:
-                seen2[word] += 1
-            else:
-                seen2[word] = 1
+        for w in words2:
+            seen2[w] = seen2.get(w, 0) + 1
 
-        for word in seen1:
-            if seen1[word] == 1 and seen2.get(word, 0) == 1:
+        for w in seen1:
+            if seen1[w] == 1 and seen2.get(w, 0) == 1:
                 count += 1
 
         return count

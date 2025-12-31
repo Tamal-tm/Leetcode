e6fac44f8af1ea37b356e5572e71a7eb6f37a4b7
Tamal Mallick <tamalmallick23@gmail.com>
2025-12-31 06:32:10 +0530

    Time: 3 ms (65.56%) | Memory: 12.6 MB (77.22%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
@@ -1,17 +1,6 @@
 class Solution(object):
     def intersection(self, nums):
-        seen = {}
-
-        # Count elements of the first array
-        for x in nums[0]:
-            seen[x] = seen.get(x, 0) + 1
-
-        # Process remaining arrays
-        for i in range(1, len(nums)):
-            current = {}
-            for x in nums[i]:
-                if x in seen:
-                    current[x] = 1
-            seen = current  # keep only common elements
-
-        return sorted(seen.keys())
+        common = set(nums[0])
+        for arr in nums[1:]:
+            common &= set(arr)
+        return sorted(common)

23333443b0a2163d453adc66b31976db11dd0336
Tamal Mallick <tamalmallick23@gmail.com>
2026-01-02 06:21:43 +0530

    Time: 46 ms (30.14%) | Memory: 13 MB (23.97%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.py
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -1,23 +1,15 @@
 class Solution(object):
     def mostFrequentEven(self, nums):
         seen = {}
-        
+        max_freq = 0
+        result = -1
+
         for n in nums:
             if n % 2 == 0:
-                if n in seen:
-                    seen[n] += 1
-                else:
-                    seen[n] = 1
-        
-        if not seen:
-            return -1
-        
-        max_freq = 0
-        answer = float('inf')
-        
-        for key, value in seen.items():
-            if value > max_freq or (value == max_freq and key < answer):
-                max_freq = value
-                answer = key
-        
-        return answer
+                seen[n] = seen.get(n, 0) + 1
+
+                if seen[n] > max_freq or (seen[n] == max_freq and n < result):
+                    max_freq = seen[n]
+                    result = n
+
+        return result

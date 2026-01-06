c32d63ef7555940c0a32abf09fbb59faef360ca9
Tamal Mallick <tamalmallick23@gmail.com>
2026-01-06 06:48:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (14.96%) - LeetSync

--- 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.py
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.py
@@ -2,11 +2,8 @@ class Solution(object):
     def sumDivisibleByK(self, nums, k):
         seen={}
         count=0
-        for i in range(len(nums)):
-            if nums[i] in seen:
-                seen[nums[i]] +=1
-            else:
-                seen[nums[i]] =1
+        for num in nums:
+            seen[num] = seen.get(num, 0) + 1
         
         for key,value in seen.items():
             if value % k == 0:

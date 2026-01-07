cb6e2117c592bd9b7da2a51d4f2b604f85e2f2ad
Tamal Mallick <tamalmallick23@gmail.com>
2026-01-07 06:31:21 +0530

    Time: 11 ms (50.00%) | Memory: 12.5 MB (64.58%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.py
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.py
@@ -1,21 +1,4 @@
 class Solution(object):
     def sortByBits(self, arr):
-        mylist = []
-        result = []
-
-        # store (count_of_1s, number)
-        for i in range(len(arr)):
-            binary = bin(arr[i])[2:]
-            ones = 0
-            for j in range(len(binary)):
-                if binary[j] == '1':
-                    ones += 1
-            mylist.append((ones, arr[i]))
-
-        # sort by count of 1s, then by number
-        mylist.sort()
-
-        for item in mylist:
-            result.append(item[1])
-
-        return result
+        arr.sort(key=lambda x: (bin(x).count('1'), x))
+        return arr

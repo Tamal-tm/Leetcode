8b88cf3ab09f6fd4faaeb3ee795e0fe2a5eedc63
Tamal Mallick <tamalmallick23@gmail.com>
2026-01-08 06:55:00 +0530

    Time: 2177 ms (5.26%) | Memory: 17.2 MB (5.26%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
@@ -1,27 +1,39 @@
 class Solution(object):
     def repeatLimitedString(self, s, repeatLimit):
-        from collections import Counter
-        
-        freq = Counter(s)
-        chars = sorted(freq.keys(), reverse=True)
-        result = []
-        
-        while chars:
-            ch = chars[0]
-            
-            use = min(freq[ch], repeatLimit)
-            result.append(ch * use)
-            freq[ch] -= use
-            
-            if freq[ch] == 0:
-                chars.pop(0)
+        new_str = ""
+        seen = {}
+
+        # sort string in descending order
+        sorted_s = "".join(sorted(s, reverse=True))
+
+        # count frequency
+        for i in range(len(sorted_s)):
+            if sorted_s[i] in seen:
+                seen[sorted_s[i]] += 1
             else:
-                if len(chars) == 1:
+                seen[sorted_s[i]] = 1
+
+        keys = sorted(seen.keys(), reverse=True)
+
+        i = 0
+        while i < len(keys):
+            ch = keys[i]
+
+            use = min(seen[ch], repeatLimit)
+            new_str += ch * use
+            seen[ch] -= use
+
+            if seen[ch] > 0:
+                # need a smaller character to break repetition
+                if i + 1 >= len(keys):
                     break
-                next_ch = chars[1]
-                result.append(next_ch)
-                freq[next_ch] -= 1
-                if freq[next_ch] == 0:
-                    chars.pop(1)
-        
-        return "".join(result)
+                next_ch = keys[i + 1]
+                new_str += next_ch
+                seen[next_ch] -= 1
+
+                if seen[next_ch] == 0:
+                    keys.pop(i + 1)
+            else:
+                i += 1
+
+        return new_str

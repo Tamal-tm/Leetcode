c525e934ce62093b2f39c283033a46412677429b
Tamal Mallick <tamalmallick23@gmail.com>
2026-01-14 12:49:48 +0530

    Time: 53 ms (59.91%) | Memory: 86.5 MB (62.11%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
@@ -5,29 +5,19 @@
 #         self.next = next
 class Solution(object):
     def swapNodes(self, head, k):
-        temp=head
-        count=1
-        while temp is not None:
-            if count == k:
-                store_val_1=temp.val
-            count +=1
-            temp=temp.next
+        first = head
+        for _ in range(k - 1):
+            first = first.next
 
-        temp=head
-        for _ in range(count-k-1):
-            temp=temp.next
-        store_val_2=temp.val
-        temp.val=store_val_1
+        kth_from_start = first
 
-        temp=head
-        count=1
-        while temp is not None:
-            if k == count:
-                temp.val=store_val_2
-            count +=1
-            temp=temp.next
-        
+        second = head
+        while first.next:
+            first = first.next
+            second = second.next
+
+        kth_from_end = second
+
+        kth_from_start.val, kth_from_end.val = kth_from_end.val, kth_from_start.val
+
         return head
-
-
-        

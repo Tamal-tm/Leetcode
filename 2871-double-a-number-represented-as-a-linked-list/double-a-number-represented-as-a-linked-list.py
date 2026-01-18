7cb721082e85c7fb8084106a69c49e1d55242521
Tamal Mallick <tamalmallick23@gmail.com>
2026-01-18 07:14:20 +0530

    Time: 128 ms (76.32%) | Memory: 19.7 MB (72.37%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
@@ -5,29 +5,41 @@
 #         self.next = next
 class Solution(object):
     def doubleIt(self, head):
-        temp=head
-        values=[]
-        count=0
-        
-        while temp is not None:
-            values.append(temp.val)
-            count+=1
-            temp=temp.next
-        single_integer = int("".join(map(str, values)))
-        main_val=2*single_integer
-        back_to_list = list(map(int, str(main_val)))
-        check=len(back_to_list)
-        temp=head
-        i=0
-        while temp is not None:
-            temp.val=back_to_list[i]
-            if i == count-1:
-                break
-            temp=temp.next
-            i+=1
 
-        new_node=ListNode(back_to_list[-1])
-        if check !=count:
-            temp.next=new_node
-            
-        return head
+        # 1) Reverse the linked list
+        prev = None
+        cur = head
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+
+        head = prev   # new head after reverse
+
+        # 2) Double each digit with carry
+        carry = 0
+        cur = head
+        last = None
+
+        while cur:
+            total = cur.val * 2 + carry
+            cur.val = total % 10
+            carry = total // 10
+            last = cur
+            cur = cur.next
+
+        # 3) If carry remains, add new node
+        if carry:
+            last.next = ListNode(carry)
+
+        # 4) Reverse again to restore order
+        prev = None
+        cur = head
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+
+        return prev

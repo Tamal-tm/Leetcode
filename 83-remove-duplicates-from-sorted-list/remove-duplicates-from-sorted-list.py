74b1899e113f6aa95d53941e5436ce04cf191d61
Tamal Mallick <tamalmallick23@gmail.com>
2026-01-19 06:43:41 +0530

    Time: 2 ms (47.41%) | Memory: 12.5 MB (23.64%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -9,8 +9,8 @@ class Solution(object):
         while curr and curr.next:
             if curr.val == curr.next.val:
                 curr.next = curr.next.next
-            else:
-                curr = curr.next
+                continue
+            curr = curr.next
         return head
 
 

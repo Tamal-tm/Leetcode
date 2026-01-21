2ac906e47b4f204495e08278bf1bd5283fe9899b
Tamal Mallick <tamalmallick23@gmail.com>
2026-01-21 06:51:56 +0530

    Time: 162 ms (75.61%) | Memory: 85.8 MB (51.22%) - LeetSync

--- 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.py
+++ 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.py
@@ -1,47 +1,33 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution(object):
     def reverseEvenLengthGroups(self, head):
-        dummy = ListNode(0)
-        dummy.next = head
-
+        dummy = ListNode(0, head)
         prev = dummy
         curr = head
-        group_size = 1
+        size = 1
 
         while curr:
-            # Step 1: count actual nodes in this group
-            count = 0
-            temp = curr
-            while temp and count < group_size:
-                temp = temp.next
-                count += 1
+            cnt = 0
+            node = curr
+            while node and cnt < size:
+                node = node.next
+                cnt += 1
 
-            # Step 2: if count is even → reverse
-            if count % 2 == 0:
-                prev_next = curr
+            if cnt & 1 == 0:
+                last = curr
                 prev_rev = None
-
-                for _ in range(count):
+                for _ in range(cnt):
                     nxt = curr.next
                     curr.next = prev_rev
                     prev_rev = curr
                     curr = nxt
-
                 prev.next = prev_rev
-                prev_next.next = curr
-                prev = prev_next
-
+                last.next = curr
+                prev = last
             else:
-                # Step 3: if odd → just move pointers
-                for _ in range(count):
+                for _ in range(cnt):
                     prev = curr
                     curr = curr.next
 
-            group_size += 1
+            size += 1
 
         return dummy.next

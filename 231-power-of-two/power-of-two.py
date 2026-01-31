213990c3128942162c352b1546c505ed59c91698
Tamal Mallick <tamalmallick23@gmail.com>
2026-01-31 10:43:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (16.06%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def isPowerOfTwo(self, n):
-        if n <= 0:
+        if n == 0:
             return False
-
-        total = 1
-        while total < n:
-            total = total * 2
-        return total == n
+        if n & (n-1) == 0:
+            return True
+        else:
+            return False

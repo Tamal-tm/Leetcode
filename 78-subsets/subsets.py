a2b6f1d86b5dfe247ee458b054c24ba51e48d7d8
Tamal Mallick <tamalmallick23@gmail.com>
2026-02-01 13:34:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (54.36%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,12 +1,16 @@
 class Solution(object):
     def subsets(self, nums):
-        n=len(nums)
-        tot_subsets=1<<n
         result=[]
-        for num in range(0,tot_subsets):
-            mylist=[]
-            for i in range(0,n):
-                if num & (1<<i)!=0:
-                    mylist.append(nums[i])
-            result.append(mylist)
+        # Using recursion
+        def solve(index,subset):
+            if index>=len(nums):
+                result.append(subset[:])
+                return
+            
+            subset.append(nums[index])
+            solve(index+1,subset)
+            subset.pop()
+            solve(index+1,subset)
+        
+        solve(0,[])
         return result

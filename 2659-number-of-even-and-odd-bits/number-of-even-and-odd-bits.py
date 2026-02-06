66b3f1a4b1869571c6a4f429e7a41f4241777889
Tamal Mallick <tamalmallick23@gmail.com>
2026-02-06 19:14:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (12.00%) - LeetSync

--- 2659-number-of-even-and-odd-bits/number-of-even-and-odd-bits.py
+++ 2659-number-of-even-and-odd-bits/number-of-even-and-odd-bits.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def evenOddBit(self, n):
-        even_count = 0
-        odd_count = 0
-
-        for i in range(16):
-            if n & (1 << i):
-                if i % 2 == 0:
-                    even_count += 1
-                else:
-                    odd_count += 1
-
+        even_count=0
+        odd_count=0
+        for i in range(0,16):
+            if i % 2 == 0:
+                if n & (1<<i):
+                    even_count +=1
+            else:
+                if n & (1<<i):
+                    odd_count +=1
+        
         return [even_count, odd_count]
+
+        

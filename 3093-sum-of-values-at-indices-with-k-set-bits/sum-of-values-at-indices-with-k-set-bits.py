7f12a8cfc762a25d57dcfb83a8eb63310f8b337d
Tamal Mallick <tamalmallick23@gmail.com>
2026-02-10 11:06:27 +0530

    Time: 2 ms (92.31%) | Memory: 12.7 MB (23.08%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.py
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.py
@@ -1,14 +1,7 @@
 class Solution(object):
     def sumIndicesWithKSetBits(self, nums, k):
-        sum_arr=0
+        sum_arr = 0
         for i in range(len(nums)):
-            count=0
-            for j in range(0,16):
-                if i & (1<<j) > 0:
-                    count +=1
-            if count == k:
-                sum_arr +=nums[i]
-        
+            if bin(i).count('1') == k:
+                sum_arr += nums[i]
         return sum_arr
-
-        

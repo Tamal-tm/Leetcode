67111e595b6b0676db3c266be3688666b045da8c
Tamal Mallick <tamalmallick23@gmail.com>
2026-02-10 13:27:11 +0530

    Time: 19 ms (86.36%) | Memory: 12.6 MB (22.73%) - LeetSync

--- 3183-find-the-k-or-of-an-array/find-the-k-or-of-an-array.py
+++ 3183-find-the-k-or-of-an-array/find-the-k-or-of-an-array.py
@@ -2,10 +2,12 @@ class Solution(object):
     def findKOr(self, nums, k):
         result = 0
         for i in range(32):
+            bit = 1 << i
             count = 0
             for n in nums:
-                if n & (1 << i):
+                if n & bit:
                     count += 1
-            if count >= k:
-                result |= (1 << i)
+                    if count == k:
+                        result |= bit
+                        break
         return result

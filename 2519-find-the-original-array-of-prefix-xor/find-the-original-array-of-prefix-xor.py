c1476817fb5ea860d5c4311980f570125be71e6a
Tamal Mallick <tamalmallick23@gmail.com>
2026-02-11 13:13:54 +0530

    Time: 35 ms (71.08%) | Memory: 30.3 MB (80.12%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.py
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def findArray(self, pref):
-        n = len(pref)
-        ans = [pref[0]]
-        
-        for i in range(1, n):
-            ans.append(pref[i] ^ pref[i - 1])
-        
-        return ans
+        for i in range(len(pref) - 1, 0, -1):
+            pref[i] ^= pref[i - 1]
+        return pref

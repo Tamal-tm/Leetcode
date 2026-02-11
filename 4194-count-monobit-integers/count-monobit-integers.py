22ac161b02bf7a3f0d511ef2f10054b45ae909f6
Tamal Mallick <tamalmallick23@gmail.com>
2026-02-11 12:50:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (89.96%) - LeetSync

--- 4194-count-monobit-integers/count-monobit-integers.py
+++ 4194-count-monobit-integers/count-monobit-integers.py
@@ -1,13 +1,10 @@
 class Solution(object):
     def countMonobit(self, n):
-        count = 0
-        
-        k = 0
-        while True:
-            val = (1 << k) - 1
-            if val > n:
-                break
-            count += 1
-            k += 1
-        
-        return count
+        current = 0
+        power = 0
+       
+        while current <= n:
+            current += 2**power
+            power += 1
+            
+        return power

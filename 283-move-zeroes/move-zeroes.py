77e50dd4d1b416337549cc5cc4177bfb40f58af2
Tamal Mallick <tamalmallick23@gmail.com>
2026-02-19 07:14:33 +0530

    Time: 4 ms (78.57%) | Memory: 14 MB (16.44%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,12 +5,10 @@ class Solution(object):
             if nums[i] == 0:
                 nums.append(0)
                 nums[i] = None  # mark removed zeros safely
-
         new_list = []
         for num in nums:
             if num is not None:
                 new_list.append(num)
-
         # Step 3: Copy back into nums (in-place update)
         nums[:] = new_list
         return nums

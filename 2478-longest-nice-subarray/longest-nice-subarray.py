2b2aa4f650871ce76d33dc637c5281d7ff5e45ff
Tamal Mallick <tamalmallick23@gmail.com>
2026-02-20 06:36:22 +0530

    Time: 103 ms (63.64%) | Memory: 21.7 MB (72.73%) - LeetSync

---
+++ 2478-longest-nice-subarray/longest-nice-subarray.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestNiceSubarray(self, nums):
+        left = 0
+        mask = 0
+        max_len = 0
+        
+        for right in range(len(nums)):
+            
+            while mask & nums[right] != 0:
+                mask ^= nums[left]
+                left += 1
+            
+            mask |= nums[right]
+            max_len = max(max_len, right - left + 1)
+        
+        return max_len

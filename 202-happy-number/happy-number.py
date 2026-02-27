960b467ed416b81c23f65367e41fcc133e6fe020
Tamal Mallick <tamalmallick23@gmail.com>
2026-02-27 18:39:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (99.63%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isHappy(self, n):
+        seen = set()  # to store numbers we've already seen
+
+        while n != 1 and n not in seen:
+            seen.add(n)
+            total = 0
+            while n > 0:
+                digit = n % 10
+                total += digit * digit
+                n //= 10
+            n = total
+
+        return n == 1

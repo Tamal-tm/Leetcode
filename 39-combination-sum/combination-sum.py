e5ff90129eabdae3585ed8a5c1a26c82172bae2b
Tamal Mallick <tamalmallick23@gmail.com>
2026-03-05 12:54:54 +0530

    Time: 23 ms (35.93%) | Memory: 12.3 MB (98.14%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+
+    def solve(self, index, total, subset, nums, target, result):
+
+        if total == target:
+            result.append(subset[:])
+            return
+
+        if total > target:
+            return
+
+        if index >= len(nums):
+            return
+
+        # PICK
+        subset.append(nums[index])
+        Sum = total + nums[index]
+        self.solve(index, Sum, subset, nums, target, result)
+
+        # BACKTRACK
+        subset.pop()
+
+        # NOT PICK
+        self.solve(index + 1, total, subset, nums, target, result)
+
+
+    def combinationSum(self, candidates, target):
+        result = []
+        self.solve(0, 0, [], candidates, target, result)
+        return result

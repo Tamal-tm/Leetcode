779e318c3a4f6eb7022b7726a4b219b7c4e64302
Tamal Mallick <tamalmallick23@gmail.com>
2026-04-02 10:39:00 +0530

    Time: 7 ms (52.38%) | Memory: 12.5 MB (82.05%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        result = []
+        depth = 0
+        
+        for ch in s:
+            if ch == "(":
+                if depth > 0:
+                    result.append(ch)
+                depth += 1
+            else:  # ch == ")"
+                depth -= 1
+                if depth > 0:
+                    result.append(ch)
+        
+        return "".join(result)

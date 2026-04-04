c893fba8603fa65212365ddb6421925ded16c61d
Tamal Mallick <tamalmallick23@gmail.com>
2026-04-04 12:21:16 +0530

    Time: 7 ms (85.13%) | Memory: 13.1 MB (85.00%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,43 +1,14 @@
-class Solution(object):
+class Solution:
     def asteroidCollision(self, asteroids):
-
-        # Size of the array
-        n = len(asteroids)
-
-        # List implementation of stack
         st = []
-
-        # Traverse all the asteroids
-        for i in range(n):
-
-            # Push the asteroid in stack if a
-            # right moving asteroid is seen
-            if asteroids[i] > 0:
-                st.append(asteroids[i])
-
-            # Else if the asteroid is moving
-            # left, perform the collisions
-            else:
-
-                # Until the right moving asteroids are
-                # smaller in size, keep on destroying them
-                while st and st[-1] > 0 and st[-1] < abs(asteroids[i]):
-
-                    # Destroy the asteroid
-                    st.pop()
-
-                # If there is right moving asteroid
-                # which is of same size
-                if st and st[-1] == abs(asteroids[i]):
-
-                    # Destroy both the asteroids
-                    st.pop()
-
-                # Otherwise, if there is no left
-                # moving asteroid, the right moving
-                # asteroid will not be destroyed
-                elif not st or st[-1] < 0:
-
-                    # Storing the array in final state
-                    st.append(asteroids[i])
+        
+        for a in asteroids:
+            while st and a < 0 < st[-1] and st[-1] < -a:
+                st.pop()
+            
+            if not st or a > 0 or st[-1] < 0:
+                st.append(a)
+            elif st[-1] == -a:
+                st.pop()
+        
         return st

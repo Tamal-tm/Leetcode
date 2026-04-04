60097c87bc9eaf0fb588d2624ee98ea61b0efda1
Tamal Mallick <tamalmallick23@gmail.com>
2026-04-04 12:19:26 +0530

    Time: 8 ms (65.23%) | Memory: 13.2 MB (53.96%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -40,6 +40,5 @@ class Solution(object):
 
                     # Storing the array in final state
                     st.append(asteroids[i])
-
         # Return the final state of asteroids
         return st

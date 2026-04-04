f2561a535cef8e491012d441401b9701c7039859
Tamal Mallick <tamalmallick23@gmail.com>
2026-04-04 11:44:54 +0530

    Time: 39 ms (58.06%) | Memory: 14.2 MB (38.38%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def nextGreaterElements(self, nums):
+        n = len(nums)
+        result = [-1] * n    # Initialize results with -1
+        stack = []           # Stack for potential NGEs
+
+        # Loop twice to simulate circular array
+        for i in range(2 * n - 1, -1, -1):
+            # Maintain decreasing stack
+            while stack and stack[-1] <= nums[i % n]:
+                stack.pop()
+            # Only set result during the first 'real' pass
+            if i < n:
+                if stack:
+                    result[i] = stack[-1]
+            # Push current value for next iteration checks
+            stack.append(nums[i % n])
+        return result

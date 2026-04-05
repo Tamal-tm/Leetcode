853d418e7f1f0d35d63b2ef4f73cb60a21b4b43f
Tamal Mallick <tamalmallick23@gmail.com>
2026-04-05 13:01:40 +0530

    Time: 14 ms (96.83%) | Memory: 19.2 MB (69.37%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -1,26 +1,29 @@
 class Solution:
     def maxScore(self, cardPoints, k):
-        """If k equals the length of the cardPoints,
-        the maximum points are achieved by picking all cards.
-        TC - O(N) (sum() function takes N time, where N is len cardPoints)
-        SC - O(1), No extra space used."""
         if k == len(cardPoints):
             return sum(cardPoints)
 
-        max_sum = 0
         left_sum = 0
         right_sum = 0
+        max_sum = 0
         n = len(cardPoints)
-        # Take all k from the left initially
+
+        # take k from left
         for i in range(k):
             left_sum += cardPoints[i]
+        
         max_sum = left_sum
 
-        # Slide: give back from left, take from right
         right_index = n - 1
+
+        # slide
         for i in range(k - 1, -1, -1):
-            left_sum -= cardPoints[i]          # remove one from the left window
-            right_sum += cardPoints[right_index]  # add one from the right tail
+            left_sum -= cardPoints[i]
+            right_sum += cardPoints[right_index]
             right_index -= 1
-            max_sum = max(max_sum, left_sum + right_sum)
+            
+            curr = left_sum + right_sum
+            if curr > max_sum:
+                max_sum = curr
+
         return max_sum
